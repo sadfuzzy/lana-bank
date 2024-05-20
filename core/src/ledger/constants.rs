6ad56fc819aba96a1977a64cbc16397bba121b05
vindard <17693119+vindard@users.noreply.github.com>
2024-05-20 15:08:41 -0400

    chore: add 'LAVA_ASSETS_ACCOUNT_ID' constant

--- core/src/ledger/constants.rs
+++ core/src/ledger/constants.rs
@@ -2,3 +2,4 @@ use uuid::{uuid, Uuid};
 
 pub(super) const LOAN_OMINBUS_EXTERNAL_ID: &str = "lava:loan-omnibus";
 pub(super) const LAVA_JOURNAL_ID: Uuid = uuid!("00000000-0000-0000-0000-000000000001");
+pub(super) const LAVA_ASSETS_ACCOUNT_ID: &str = "lava:assets";
