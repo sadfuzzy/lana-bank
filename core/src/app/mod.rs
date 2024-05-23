2ac4769a9b7f9b01be40a10f53ae8daa17ee3e0a
bodymindarts <justin@galoy.io>
2024-05-23 11:46:36 +0200

    chore: delete LineOfCredit stuff

--- core/src/app/mod.rs
+++ core/src/app/mod.rs
@@ -7,7 +7,6 @@ use crate::{
     fixed_term_loan::FixedTermLoans,
     job::{JobRegistry, Jobs},
     ledger::Ledger,
-    line_of_credit::LineOfCreditContracts,
     user::Users,
 };
 
@@ -19,7 +18,6 @@ pub struct LavaApp {
     _pool: PgPool,
     _jobs: Jobs,
     fixed_term_loans: FixedTermLoans,
-    line_of_credit_contracts: LineOfCreditContracts,
     users: Users,
     ledger: Ledger,
 }
@@ -30,28 +28,20 @@ impl LavaApp {
         let ledger = Ledger::init(config.ledger).await?;
         let users = Users::new(&pool, &ledger);
         let mut fixed_term_loans = FixedTermLoans::new(&pool, &mut registry, &ledger);
-        let mut line_of_credit_contracts =
-            LineOfCreditContracts::new(&pool, &mut registry, &ledger);
         let mut jobs = Jobs::new(&pool, config.job_execution, registry);
         fixed_term_loans.set_jobs(&jobs);
-        line_of_credit_contracts.set_jobs(&jobs);
         jobs.start_poll().await?;
         Ok(Self {
             _pool: pool,
             _jobs: jobs,
             users,
             fixed_term_loans,
-            line_of_credit_contracts,
             ledger,
         })
     }
 
     pub fn fixed_term_loans(&self) -> &FixedTermLoans {
         &self.fixed_term_loans
-    }
-
-    pub fn line_of_credit_contracts(&self) -> &LineOfCreditContracts {
-        &self.line_of_credit_contracts
     }
 
     pub fn users(&self) -> &Users {
--- core/src/line_of_credit/error.rs
+++
@@ -1,9 +0,0 @@
-use thiserror::Error;
-
-#[derive(Error, Debug)]
-pub enum LineOfCreditContractError {
-    #[error("LineOfCreditContractError - Sqlx: {0}")]
-    Sqlx(#[from] sqlx::Error),
-    #[error("LineOfCreditContractError - EntityError: {0}")]
-    EntityError(#[from] crate::entity::EntityError),
-}
