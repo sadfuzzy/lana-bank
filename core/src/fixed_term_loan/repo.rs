4a994ca2592d544d4cd2c456dc95bbba158db3b4
bodymindarts <justin@galoy.io>
2024-05-23 11:47:38 +0200

    chore: persist user_id in loan

--- core/src/fixed_term_loan/repo.rs
+++ core/src/fixed_term_loan/repo.rs
@@ -18,11 +18,11 @@ impl FixedTermLoanRepo {
         tx: &mut Transaction<'_, Postgres>,
         new_loan: NewFixedTermLoan,
     ) -> Result<EntityUpdate<FixedTermLoan>, FixedTermLoanError> {
-        let id = new_loan.id;
         sqlx::query!(
-            r#"INSERT INTO fixed_term_loans (id)
-            VALUES ($1)"#,
-            id as FixedTermLoanId,
+            r#"INSERT INTO fixed_term_loans (id, user_id)
+            VALUES ($1, $2)"#,
+            new_loan.id as FixedTermLoanId,
+            new_loan.user_id as UserId,
         )
         .execute(&mut **tx)
         .await?;
