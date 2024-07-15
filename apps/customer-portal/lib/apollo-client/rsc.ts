4b53ebf556a16efabb22307154d78c4f684a0b2a
Siddharth <siddharthtiwarikreplind@gmail.com>
2024-07-15 12:46:51 +0530

    chore: log apollo request (#191)

--- apps/customer-portal/lib/apollo-client/rsc.ts
+++ apps/customer-portal/lib/apollo-client/rsc.ts
@@ -1,18 +1,50 @@
-import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client"
+import { ApolloClient, ApolloLink, HttpLink, InMemoryCache } from "@apollo/client"
 import { registerApolloClient } from "@apollo/experimental-nextjs-app-support"
 import { headers } from "next/headers"
 
 import { env } from "@/env"
 
+const customRequest = async (
+  uri: RequestInfo | URL,
+  options?: RequestInit,
+): Promise<Response> => {
+  console.log(`Request to ${uri}`)
+  console.log(`Request method: ${options?.method}`)
+  console.log(`Request headers: ${JSON.stringify(options?.headers)}`)
+  if (options?.body) {
+    console.log(`Request body: ${options.body}`)
+  }
+
+  try {
+    const response = await fetch(uri, options)
+    console.log(`Response status: ${response.status}`)
+    console.log(
+      `Response headers: ${JSON.stringify(Object.fromEntries(response.headers.entries()))}`,
+    )
+
+    return response
+  } catch (error) {
+    console.error("Fetch error:", error)
+    throw error
+  }
+}
+
 export const { getClient } = registerApolloClient(() => {
+  console.log("nextjs headers start -----")
+  console.log(JSON.stringify(headers(), null, 2))
+  console.log("nextjs headers end -----")
+
   return new ApolloClient({
     cache: new InMemoryCache(),
-    link: new HttpLink({
-      uri: `${env.NEXT_PUBLIC_CORE_URL}/graphql`,
-      fetchOptions: { cache: "no-store" },
-      headers: {
-        authorization: headers().get("authorization") || "",
-      },
-    }),
+    link: ApolloLink.from([
+      new HttpLink({
+        uri: `${env.NEXT_PUBLIC_CORE_URL}/graphql`,
+        fetchOptions: { cache: "no-store" },
+        fetch: customRequest,
+        headers: {
+          authorization: headers().get("authorization") || "",
+        },
+      }),
+    ]),
   })
 })
