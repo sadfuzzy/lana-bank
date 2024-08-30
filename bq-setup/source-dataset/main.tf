1feca5ecac229f9373fa687a73a91e46fb440c44
bodymindarts <justin@galoy.io>
2024-08-30 13:21:57 +0200

    chore: expose sa email

--- bq-setup/source-dataset/main.tf
+++ bq-setup/source-dataset/main.tf
@@ -22,6 +22,10 @@ output "service_account_key_base64" {
   value   = google_service_account_key.bq_access_sa_key.private_key
 }
 
+output "service_account_email" {
+  value   = google_service_account_key.bq_access_sa_key.email
+}
+
 output "dataset_id" {
   value = google_bigquery_dataset.dataset.dataset_id
 }
