b89c5f2078181a53bf0daf837b27158365c5115b
bodymindarts <justin@galoy.io>
2024-09-26 16:31:13 +0200

    fix: add prod to includes/env.js

--- includes/envs.js
+++ includes/envs.js
@@ -10,13 +10,20 @@ const staging = {
   importSchema: "volcano_staging_dataset",
 }
 
+const prod = {
+  name: "volcano-prod",
+  database: "volcano-prod",
+  importSchema: "volcano_prod_dataset",
+}
+
 const paramsByName = {
   [dev.name]: dev,
   [staging.name]: staging,
+  [prod.name]: prod,
 }
 
 module.exports = {
-  all: [dev, staging],
+  all: [dev, staging, prod],
   current: dataform.projectConfig.vars.executionEnv,
   currentDatabase: paramsByName[dataform.projectConfig.vars.executionEnv].database,
   currentImportSchema: paramsByName[dataform.projectConfig.vars.executionEnv].importSchema
