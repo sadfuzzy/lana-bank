3c17b2b11c1e83d0e7046573b58d97dfb370dd82
Jose I. Rojas Echenique <jir@galoy.io>
2024-10-09 19:41:38 -0600

    feat: initial work on integrating sumsub data in to dataform pipeline (#699)

--- definitions/sources/lava.js
+++ definitions/sources/lava.js
@@ -1,4 +1,4 @@
-const tables = ["loans", "loan_events", "customer_events", "price_cents_btc"]
+const tables = ["loans", "loan_events", "customer_events", "price_cents_btc", "sumsub_applicants"]
 
 envs.all.forEach((env) => {
   tables.forEach((table) => {
