496eda20319bd99e2e005c8507dd8826f2c57362
Jose I. Rojas Echenique <jir@galoy.io>
2024-10-21 23:31:38 -0600

    feat: requlatory reporting for credit facilities (#815)

--- definitions/sources/lava.js
+++ definitions/sources/lava.js
@@ -1,4 +1,4 @@
-const tables = ["loans", "loan_events", "customer_events", "price_cents_btc", "sumsub_applicants"]
+const tables = ["loans", "loan_events", "customer_events", "price_cents_btc", "sumsub_applicants", "credit_facility_events"]
 
 envs.all.forEach((env) => {
   tables.forEach((table) => {
