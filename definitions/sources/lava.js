04ee3b6cd0ca2cbfedab300fbc4200b2ff24f925
Jose I. Rojas Echenique <jir@galoy.io>
2024-11-06 06:19:28 -0700

    feat: generate saldo_cuenta.xml according to NPB4-16 (#932)
    * feat: expand account set tree
    
    * feat: account and account set names
    
    * feat: add balances for saldo_cuenta.xml
    
    * fix: whitespace

--- definitions/sources/lava.js
+++ definitions/sources/lava.js
@@ -1,4 +1,4 @@
-const tables = ["loans", "loan_events", "customer_events", "price_cents_btc", "sumsub_applicants", "credit_facility_events"]
+const tables = ["loans", "loan_events", "customer_events", "price_cents_btc", "sumsub_applicants", "credit_facility_events", "accounts", "account_sets", "account_set_members", "balances"]
 
 envs.all.forEach((env) => {
   tables.forEach((table) => {
