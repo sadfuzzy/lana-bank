e12406322c83285a9e5b5cdf6b2ca35e2eb8bcea
Vaibhav <thevaibhavdixit@gmail.com>
2025-02-04 16:47:33 +0530

    chore: remove app/customer (#1352)

--- lana/app/src/customer/config.rs
+++
@@ -1,9 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-use super::KratosConfig;
-
-#[derive(Debug, Default, Clone, Deserialize, Serialize)]
-pub struct CustomerConfig {
-    #[serde(default)]
-    pub kratos: KratosConfig,
-}
