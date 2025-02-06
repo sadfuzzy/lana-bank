54f8b6f0b78b25eb06b27d1f284bbcc52be14cac
Sandipan <hey@sandipan.dev>
2025-02-06 09:33:19 +0530

    ci: pipeline cust portal (#1372)

--- ci/tasks/prep-release-apps.sh
+++ ci/tasks/prep-release-apps.sh
@@ -9,3 +9,11 @@ pushd admin-panel-src/apps/admin-panel
 cat <<EOF >> .env
 NEXT_PUBLIC_APP_VERSION=${version}
 EOF
+
+popd
+
+pushd customer-portal-src/apps/customer-portal
+
+cat <<EOF >> .env
+NEXT_PUBLIC_APP_VERSION=${version}
+EOF
