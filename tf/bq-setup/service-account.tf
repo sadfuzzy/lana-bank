a2bc988175ee25be9f2c71dd0a448cca3578ade3
José <jir@galoy.io>
2025-02-20 01:51:58 -0700

    chore(reporting): enable INFORMATION_SCHEMA.JOBS view (#1472)
    This is necessary to see how our dbt queries perform and identify any
    problematic queries.  Recently started seeing some concerning performance
    with some of the queries used in the models and I want to get more information.

--- tf/bq-setup/service-account.tf
+++ tf/bq-setup/service-account.tf
@@ -1,7 +1,7 @@
 resource "google_service_account" "bq_access_sa" {
   project      = local.gcp_project
   account_id   = local.sa_account_id
-  display_name = "Serviae Account for lana-bank BigQuery access"
+  display_name = "Service Account for lana-bank BigQuery access"
 }
 
 resource "google_service_account_key" "bq_access_sa_key" {
@@ -11,5 +11,11 @@ resource "google_service_account_key" "bq_access_sa_key" {
 resource "google_project_iam_member" "bq_jobuser" {
   project = local.gcp_project
   role    = "roles/bigquery.jobUser"
+  member  = "serviceAccount:${google_service_account.bq_access_sa.email}"
+}
+
+resource "google_project_iam_member" "bq_resourceviewer" {
+  project = local.gcp_project
+  role    = "roles/bigquery.resourceViewer"
   member  = "serviceAccount:${google_service_account.bq_access_sa.email}"
 }
