098052db36c3fde35382481b96b1243bedce341b
Jiří Jakeš <jiri@jirijakes.com>
2025-04-02 16:51:35 +0800

    chore: Fix textual representation of JournalError (#1742)
    During renaming of General Ledger into Journal, this was omitted.

--- core/accounting/src/journal/error.rs
+++ core/accounting/src/journal/error.rs
@@ -2,18 +2,18 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum JournalError {
-    #[error("GeneralLedgerError - Sqlx: {0}")]
+    #[error("JournalError - Sqlx: {0}")]
     Sqlx(#[from] sqlx::Error),
-    #[error("GeneralLedgerError - CalaLedger: {0}")]
+    #[error("JournalError - CalaLedger: {0}")]
     CalaLedger(#[from] cala_ledger::error::LedgerError),
-    #[error("GeneralLedgerError - CalaEntryError: {0}")]
+    #[error("JournalError - CalaEntryError: {0}")]
     CalaEntry(#[from] cala_ledger::entry::error::EntryError),
-    #[error("GeneralLedgerError - AuthorizationError: {0}")]
+    #[error("JournalError - AuthorizationError: {0}")]
     AuthorizationError(#[from] authz::error::AuthorizationError),
-    #[error("GeneralLedgerError - UnexpectedCurrency")]
+    #[error("JournalError - UnexpectedCurrency")]
     UnexpectedCurrency,
-    #[error("GeneralLedgerError - ConversionError: {0}")]
+    #[error("JournalError - ConversionError: {0}")]
     ConversionError(#[from] core_money::ConversionError),
-    #[error("GeneralLedgerError - ParseCurrencyError: {0}")]
+    #[error("JournalError - ParseCurrencyError: {0}")]
     ParseCurrencyError(#[from] cala_ledger::ParseCurrencyError),
 }
