1235d0808174c18d31ef7910a334afa1b6de3dff
nicolasburtey <nicolasburtey@users.noreply.github.com>
2025-04-03 09:44:11 +0100

    chore: use --turbo for nextjs to lunch faster (#1728)
    * chore: use --turbo for nextjs to lunch faster
    
    * fix: svg and build issues
    
    * fix: build cmd in customer portal
    
    ---------
    
    Co-authored-by: siddharth <siddharthtiwarikreplind@gmail.com>

--- apps/customer-portal/next.config.mjs
+++ apps/customer-portal/next.config.mjs
@@ -10,6 +10,17 @@ const nextConfig = {
   transpilePackages: ["@lana/web"],
   output: "standalone",
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  experimental: {
+    turbo: {
+      enabled: true,
+      rules: {
+        "**/*.svg": {
+          loaders: ["@svgr/webpack"],
+          as: "*.js",
+        },
+      },
+    },
+  },
 }
 
 export default nextConfig
