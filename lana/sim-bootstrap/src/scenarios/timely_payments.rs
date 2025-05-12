d48c0cb8c9686b0915a404e168235c2f0179a991
Sandipan <hey@sandipan.dev>
2025-05-12 18:12:44 +0530

    chore: change flow of sim from event based to parallel time loop (#1983)
    Co-authored-by: Sandipan Dey <sandipndev@Sandipans-MacBook-Air.local>

--- lana/sim-bootstrap/src/scenarios/timely_payments.rs
+++ lana/sim-bootstrap/src/scenarios/timely_payments.rs
@@ -1,7 +1,12 @@
 use futures::StreamExt;
-use lana_app::{app::LanaApp, credit::CreditFacilityHistoryEntry::*, primitives::*};
+use lana_app::{
+    app::LanaApp,
+    credit::{self, CreditFacilityHistoryEntry::*},
+    primitives::*,
+};
 use lana_events::{CoreCreditEvent, LanaEvent};
 use rust_decimal_macros::dec;
+use tokio::sync::mpsc;
 
 use crate::helpers;
 
@@ -19,8 +24,6 @@ pub async fn timely_payments_scenario(sub: Subject, app: &LanaApp) -> anyhow::Re
         .credit()
         .initiate(&sub, customer_id, deposit_account_id, cf_amount, cf_terms)
         .await?;
-
-    let mut n_disbursal = 0;
 
     let mut stream = app.outbox().listen_persisted(None).await?;
     while let Some(msg) = stream.next().await {
@@ -41,36 +44,29 @@ pub async fn timely_payments_scenario(sub: Subject, app: &LanaApp) -> anyhow::Re
                 app.credit()
                     .initiate_disbursal(&sub, cf.id, UsdCents::try_from_usd(dec!(1_000_000))?)
                     .await?;
+
+                break;
             }
+            _ => {}
+        }
+    }
+
+    let (tx, rx) = mpsc::channel::<UsdCents>(32);
+    let sim_app = app.clone();
+    tokio::spawn(async move {
+        do_timely_payments(sub, sim_app, cf.id, rx)
+            .await
+            .expect("timely payments failed");
+    });
+
+    while let Some(msg) = stream.next().await {
+        match &msg.payload {
             Some(LanaEvent::Credit(CoreCreditEvent::ObligationDue {
                 credit_facility_id: id,
                 amount,
                 ..
             })) if { cf.id == *id && amount > &UsdCents::ZERO } => {
-                app.credit()
-                    .record_payment(&sub, *id, *amount, sim_time::now().date_naive())
-                    .await?;
-                let facility = app
-                    .credit()
-                    .find_by_id(&sub, *id)
-                    .await?
-                    .expect("cf exists");
-
-                n_disbursal += 1;
-                if n_disbursal == 3 {
-                    let total_outstanding_amount = app.credit().outstanding(&facility).await?;
-                    if !total_outstanding_amount.is_zero() {
-                        app.credit()
-                            .record_payment(
-                                &sub,
-                                facility.id,
-                                total_outstanding_amount,
-                                sim_time::now().date_naive(),
-                            )
-                            .await?;
-                    }
-                    app.credit().complete_facility(&sub, facility.id).await?;
-                }
+                tx.send(*amount).await?;
             }
             Some(LanaEvent::Credit(CoreCreditEvent::FacilityCompleted { id, .. })) => {
                 if cf.id == *id {
@@ -90,7 +86,7 @@ pub async fn timely_payments_scenario(sub: Subject, app: &LanaApp) -> anyhow::Re
 
     let history = app
         .credit()
-        .history::<lana_app::credit::CreditFacilityHistoryEntry>(&sub, cf.id)
+        .history::<credit::CreditFacilityHistoryEntry>(&sub, cf.id)
         .await?;
 
     let (disbursals_and_interests, repayments) =
@@ -99,8 +95,61 @@ pub async fn timely_payments_scenario(sub: Subject, app: &LanaApp) -> anyhow::Re
             Payment(_) => (di, p + 1),
             _ => (di, p),
         });
-    assert_eq!(disbursals_and_interests, 3);
-    assert_eq!(repayments, 3);
+    assert_eq!(disbursals_and_interests, 6);
+    assert_eq!(repayments, 6);
+
+    Ok(())
+}
+
+async fn do_timely_payments(
+    sub: Subject,
+    app: LanaApp,
+    id: CreditFacilityId,
+    mut obligation_amount_rx: mpsc::Receiver<UsdCents>,
+) -> anyhow::Result<()> {
+    let one_month = std::time::Duration::from_secs(30 * 24 * 60 * 60);
+
+    for _ in 0..3 {
+        sim_time::sleep(one_month).await;
+
+        let amount = obligation_amount_rx
+            .recv()
+            .await
+            .expect("obligation not received");
+
+        app.credit()
+            .record_payment(&sub, id, amount, sim_time::now().date_naive())
+            .await?;
+    }
+
+    let facility = app.credit().find_by_id(&sub, id).await?.unwrap();
+    let total_outstanding = app.credit().outstanding(&facility).await?;
+    if !total_outstanding.is_zero() {
+        app.credit()
+            .record_payment(
+                &sub,
+                facility.id,
+                total_outstanding,
+                sim_time::now().date_naive(),
+            )
+            .await?;
+    }
+
+    const MAX_RETRIES: usize = 15;
+    for attempt in 0..MAX_RETRIES {
+        match app.credit().complete_facility(&sub, facility.id).await {
+            Ok(_) => {
+                break;
+            }
+            Err(_) if attempt + 1 < MAX_RETRIES => {
+                tokio::time::sleep(std::time::Duration::from_secs(2)).await;
+                continue;
+            }
+            Err(e) => {
+                panic!("Failed to complete facility: {:?}", e);
+            }
+        }
+    }
 
     Ok(())
 }
