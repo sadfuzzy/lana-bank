c8aa24726dbeb486137e8eb9c68dd48a7e30857c
Justin Carter <justin@galoy.io>
2025-06-10 10:37:33 +0200

    Revert "chore: bump python to 3.12 for faster build (#2155)" (#2164)
    This reverts commit 5ac4414fe7bcf1ec837a8844ca5a495f0abd852b.

--- meltano.nix
+++ meltano.nix
@@ -1,9 +1,9 @@
 {
   lib,
-  python3,
+  python311,
   fetchPypi,
 }:
-python3.pkgs.buildPythonApplication rec {
+python311.pkgs.buildPythonApplication rec {
   pname = "meltano";
   version = "3.7.8";
   pyproject = true;
@@ -13,11 +13,11 @@ python3.pkgs.buildPythonApplication rec {
     hash = "sha256-dwYJzgqa4pYuXR2oadf6jRJV0ZX5r+mpSE8Km9lzDLI=";
   };
 
-  nativeBuildInputs = with python3.pkgs; [
+  nativeBuildInputs = with python311.pkgs; [
     hatchling
   ];
 
-  propagatedBuildInputs = with python3.pkgs; [
+  propagatedBuildInputs = with python311.pkgs; [
     click
     pyyaml
     requests
@@ -34,7 +34,7 @@ python3.pkgs.buildPythonApplication rec {
     structlog
     watchdog
     click-default-group
-    fasteners
+    (fasteners.overridePythonAttrs (old: {doCheck = false;}))
     croniter
     pathvalidate
     click-didyoumean
